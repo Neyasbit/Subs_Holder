6eaaf2442c84a5b8c21c65c0116e96b4bd7d3c14
oleg <neyasbit@gmail.com>
2023-04-04 20:44:47 +0700

    changed the plugin for the subfolder

--- build-logic/convention-plugin/src/main/kotlin/SettingsConventionPlugin.kt
+++ build-logic/convention-plugin/src/main/kotlin/SettingsConventionPlugin.kt
@@ -1,12 +1,13 @@
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
 import org.gradle.api.initialization.resolve.RepositoriesMode
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+
 
 @Suppress("UnstableApiUsage", "unused")
 internal class SettingsConventionPlugin : Plugin<Settings> {
-
-    private val codeQualityDirName = "codequality"
-    private val buildDirName = "build"
 
     override fun apply(target: Settings) = target.run {
 
@@ -19,12 +20,41 @@ internal class SettingsConventionPlugin : Plugin<Settings> {
         }
         val filterProjectDirectory = rootDir
             .listFiles()
-            ?.filter {
-                it.isDirectory
-                        && !it.isHidden &&
-                        it.name != codeQualityDirName &&
-                        !it.name.startsWith(buildDirName)
+            ?.filter { file -> file.isDirectory && !file.isHidden }
+            ?.filter { file ->
+                file
+                    .listFiles()
+                    ?.filter { subFile ->
+                        FilterProjectDirContract.SubProjectFiler().directoryFilter(file, subFile)
+                    }?.forEach { subFile ->
+                        include(":${file.name}:${subFile.name}")
+                    }
+                FilterProjectDirContract.RootDir().directoryFilter(file)
             }
             ?.forEach { file -> include(file.name) }
+    }
+}
+
+internal interface FilterProjectDirContract {
+
+
+    val buildGradleFileName get() = "/build.gradle.kts"
+
+    fun directoryFilter(file: File, subFile: File? = null): Boolean
+
+    class RootDir : FilterProjectDirContract {
+        override fun directoryFilter(file: File, subFile: File?) =
+            Files.exists(Paths.get(file.path + buildGradleFileName))
+
+    }
+
+    class SubProjectFiler : FilterProjectDirContract {
+        override fun directoryFilter(file: File, subFile: File?): Boolean {
+            val buildDirName = "build"
+
+            return !file.name.startsWith(buildDirName) &&
+                    Files.exists(Paths.get(subFile?.path + buildGradleFileName))
+
+        }
     }
 }
