b937db18fcf694abbe2cfe03e2d6e603c03a498a
oleg <neyasbit@gmail.com>
2023-05-23 21:27:42 +0700

    remade task for static analyzer

--- build-logic/convention-plugin/src/main/kotlin/helpers/ConfigureKotlinAndroid.kt
+++ build-logic/convention-plugin/src/main/kotlin/helpers/ConfigureKotlinAndroid.kt
@@ -1,7 +1,6 @@
 package helpers
 
 import com.android.build.api.dsl.CommonExtension
-import com.android.build.gradle.internal.tasks.factory.dependsOn
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionAware
@@ -47,16 +46,12 @@ internal fun Project.configureKotlinAndroid(
         /**
          * The task runs code analyzers before building the project
          */
-         tasks
-            .getByPath("preBuild")
-            .dependsOn("ktlintFormat")
 
-        /**
-         * @see SpotlessAutoPreBuildTask
-         */
-        tasks
-            .register("autoCheckAndApplySpotless")
-            .dependsOn("build")
+        tasks.named("preBuild") {
+            dependsOn("ktlintFormat")
+            dependsOn("spotlessCheck")
+            dependsOn("spotlessApply")
+        }
 
     }
 }
